8d738914711d8a4fdfab12483b1aa913370771ed
alexpatney <70240335+alexpatney@users.noreply.github.com>
2026-03-08 20:55:52 -0400

    Update Bond.java

--- src/Bond.java
+++ src/Bond.java
@@ -1,3 +1,56 @@
+/**
+* Bond is a child class of FinancialAsset
+*/
 public class Bond extends FinancialAsset{
   private double faceValue;
-  private double coupon;
+  private double couponRate;
+  private int yearsToMaturity;
+
+  public Bond(String name, double currentValue, int riskRating,
+              double faceValue, double couponRate, int yearsToMaturity){
+    super(name, currentValue, riskRating);
+    this.faceValue = faceValue;
+    this.couponRate = couponRate;
+    this.yearsToMaturity = yearsToMaturity;
+  }
+
+  public double getFaceValue(){
+    return faceValue;
+  }
+
+  public void setFaceValue(double faceValue){
+    this.faceValue = faceValue;
+  }
+
+  public double getCouponRate(){
+    return couponRate;
+  }
+
+  public void setCouponRate(double couponRate){
+    this.couponRate = couponRate;
+  }
+
+  public int getYearsToMaturity(){
+    return yearsToMaturity;
+  }
+
+  public void setYearsToMaturity(int yearsToMaturity){
+    this.yearsToMaturity = yearsToMaturity;
+  }
+
+  @Override
+  public double calculateReturn(){
+    return faceValue * couponRate;
+  }
+
+  @Override
+  public void displayInfo(){
+    System.out.println("--- Bond ---");
+    super.displayInfo();
+    System.out.printf("Face Value:      $%.2f%n", faceValue);
+    System.out.printf("Coupon Rate:     %.2f%%%n", couponRate * 100);
+    System.out.println("Years To Maturity: " + yearsToMaturity);
+    System.out.printf("Expected Return: $%.2f%n", calculateReturn());
+    System.out.println();
+  }
+}
